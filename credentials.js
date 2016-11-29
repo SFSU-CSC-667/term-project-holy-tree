d95637707b04104728c9d7a6acfd804d64cb94f4
Thao <tluu4@mail.sfsu.edu>
2016-11-28 18:57:07 -0800

    remove creds

--- credentials.js
+++
@@ -1,6 +0,0 @@
-var credentials = {
-	GOOGLE_CLIENT_ID: "254423767125-c6fdo996jkh1jmmjd4svp1aua2ifj1k2.apps.googleusercontent.com",
-	GOOGLE_CLIENT_SECRET: "GY14EdB8s0YN58YsBiG_XUP4",
-};
-
-module.exports = credentials;
