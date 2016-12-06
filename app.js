06bcbf059804cc4ed5ae2af242b0a3f906304ea6
Brook Thomas <brookthomas@gmail.com>
2016-12-05 17:19:26 -0800

    fixed playerjoined socket downstream

--- app.js
+++ app.js
@@ -25,7 +25,15 @@ io.on('connection', function(socket){
   // Socket.io Intializations and Config
   socket.on('subscribe to lobby', function(data) {
     socket.join(data.lobby);
-    io.to(data.lobby).emit('player joined', data);
+    
+    io.to(data.lobby).emit('player joined', { 
+      player_count: 3 
+    });
+    
+    io.to(data.lobby).emit('chat message', {
+      message: `${data.name} has joined the lobby`,
+      user_name: 'WerewolfApp'
+    });
   });
 
   socket.on('chat message', function(data) {
@@ -34,17 +42,7 @@ io.on('connection', function(socket){
       user_name: data.user_name
     });
   });
-
-  socket.on('player joined', function(data) {
-    io.to(data.lobby).emit('player joined', { 
-      player_count: 3 
-    });
-    io.to(data.lobby).emit('chat message', {
-      message: `${data.name} has joined the lobby`,
-      user_name: 'WerewolfApp'
-    });
-  });
-
+  
 });
 
 
