9fcd8ec287f664024b41dd01b538fa9b1b8a9fba
Brook Thomas <brookthomas@gmail.com>
2016-12-05 18:52:47 -0800

    incremental changes pending fito's push

--- models/Lobby.js
+++ models/Lobby.js
@@ -7,7 +7,7 @@ class Lobby {
 
     findAvailable () {
       return db.one(
-        "SELECT id FROM lobbies WHERE visible = $1 and player_count < $2 LIMIT 1;",
+        "SELECT MIN(id) FROM lobbies WHERE visible = $1 and player_count < $2;",
         [true, Lobby.MAX_PLAYERS]
       )
     }
@@ -16,6 +16,14 @@ class Lobby {
       return db.one(
         "INSERT INTO lobbies (visible, player_count) VALUES ($1, $2) RETURNING id;",
         [true, 0]
+      )
+    }
+
+    incrementPlayerCount (lobby_id) {
+      return db.one(
+        "UPDATE lobbies SET player_count = player_count + 1 WHERE id = $1 RETURNING player_count;",
+        [lobby_id],
+        data => data.player_count
       )
     }
 
