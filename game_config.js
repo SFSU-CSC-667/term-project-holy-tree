964f4e4e0850dc31acefe7f001c366317928a14d
Brook Thomas <brookthomas@gmail.com>
2016-12-10 19:02:29 -0800

    updated game config with order

--- game_config.js
+++ game_config.js
@@ -2,12 +2,14 @@ const game_config =
 {   
     3: {
         roles: ['werewolf', 'werewolf', 'seer', 'villager', 'villager'],
+        order: ['seer'],
         night_duration: 15,
         day_duration: 140
     },
 
     4: {
         roles: ['werewolf', 'werewolf', 'seer', 'villager', 'villager', 'villager'],
+        order: ['seer'],
         night_duration: 15,
         day_duration: 180
     }
