551c6fe4234dc0267bf38c747bf9d4cb1e03846d
Fito von Zastrow <fito@paragil.com>
2016-12-10 19:23:43 -0800

    Sends message to individual users when game is ready to start

--- routes/socketInit.js
+++ routes/socketInit.js
@@ -1,33 +1,50 @@
 const models = require('../models/models')
 
-const subscribeUser = ( user_id, game_id ) => {
-  let player_count, user;
-
-  return models.game.incrementPlayerCount( game_id )
-    .then( count => { player_count = count; })
-    .then( _ => models.user.findByID( user_id ))
-    .then( user_data => { user = user_data; })
-    .then( _ => ({ player_count, user }));
-}
-
 const socketInit = io => {
+
+  const MAX_PLAYERS = 3;
+  const user_sockets = {};
+
+  const subscribeUser = ( user_id, game_id ) => {
+    let player_count, user;
+    return models.game.incrementPlayerCount( game_id )
+      .then( count => { player_count = count; })
+      .then( _ => models.user.findByID( user_id ))
+      .then( user_data => { user = user_data; })
+      .then( _ => ({ player_count, user }));
+  }
+
+  const notify_individial_user = ( user_id ) => {
+    io.sockets.connected[ user_sockets[user_id] ]
+      .emit('game starting', { time: 20, phase: 'Night', role: 'Villager', user_id: user_id });
+      // get the role information from an object
+  }
+
   io.on('connection', socket => {
-
-    // Socket.io Intializations and Config
     socket.on('subscribe to game', subscription => {
-      socket.join( subscription.game_id );
+      const game_id = subscription.game_id;
+      const user_id = subscription.user_id;
 
-      subscribeUser( subscription.user_id, subscription.game_id )
+      if ( !user_sockets.hasOwnProperty( user_id ) ) {
+        user_sockets[user_id] = socket.id;
+      }
+
+      socket.join( game_id );
+
+      subscribeUser( user_id, game_id )
         .then( args => {
-            console.log(args);
-            io.to(subscription.game_id).emit('player joined', { player_count: args.player_count, name: args.user.name, profile_pic: args.user.profile_pic, user_id: args.user.id });
-            io.to(subscription.game_id).emit('chat message', { message: `${args.user.name} has joined the game`, name: 'GAME' });
+          io.to( game_id ).emit('player joined', { player_count: args.player_count, name: args.user.name, profile_pic: args.user.profile_pic, user_id: args.user.id });
+          io.to( game_id ).emit('chat message', { message: `${args.user.name} has joined the game`, name: 'GAME' });
+          if( args.player_count == MAX_PLAYERS ) {
+            models.game.getUsers( game_id )
+              .then( users => { users.forEach( user => notify_individial_user( user.id ))})
+          }
         })
         .catch( error => { console.log(error) });
-    })
+    });
 
     socket.on('chat message', data => {
-      io.to(data.game).emit('chat message', {
+      io.to(data.game_id).emit('chat message', {
         message: data.message,
         name: data.name
       });
